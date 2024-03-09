522c0e4f26a5f19f7e844679f13982fb4ae86c5c
u1990700 <u1990700@campus.udg.edu>
2024-03-09 20:18:07 +0100

    Menu_cambios
    Menu mejorado a nivel visual

--- js/menu.js
+++ js/menu.js
@@ -11,28 +11,3 @@ saves.on('click', function () { console.error("Opció no implementada")} );
 exit.on('click', function () { console.warn("No es pot sortir!")} );
 
 
-
-
-/*
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        window.location.assign("./html/game.html");
-    });
-
-    document.getElementById('options').addEventListener('click', 
-    function(){
-        window.location.assign("./html/options.html");
-    });
-
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        console.error("Opció no implementada");
-    });
-
-    document.getElementById('exit').addEventListener('click', 
-    function(){
-        console.warn("No es pot sortir!");
-    });
-});
-*/
